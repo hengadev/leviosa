655b04f734a21078ed6bfcc10cc56bd9225118ea
gary <henry.gary@hotmail.com>
2024-01-30 20:04:15 +0100

    fix: rename StubStore to Store

--- backend/cmd/event-reservation-app/main.go
+++ backend/cmd/event-reservation-app/main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"fmt"
 	"github.com/GaryHY/event-reservation-app/internal/api"
+	"github.com/GaryHY/event-reservation-app/internal/database"
+	"log"
 	"net/http"
 )
 
 func main() {
+	// NOTE: La BDD n'a rien dedans donc...
+	store, err := sqlite.NewStore("db.sqlite")
+	if err != nil {
+		log.Fatal("Cannot connect to the database")
+	}
+	server := api.NewServer(store)
+
 	fmt.Println("Running server on port 5000...")
-	http.ListenAndServe(":5000", http.HandlerFunc(api.PlayerServer))
+	http.ListenAndServe(":5000", server)
 }
--- backend/internal/database/sqlite.go
+++ backend/internal/database/sqlite.go
@@ -7,11 +7,11 @@ import (
 	"log"
 )
 
-type StubStore struct {
-	db *sql.DB
+type Store struct {
+	DB *sql.DB
 }
 
-func NewStubStore(connStr string) (*StubStore, error) {
+func NewStore(connStr string) (*Store, error) {
 	if connStr == "" {
 		connStr = ":memory:"
 	}
@@ -19,20 +19,28 @@ func NewStubStore(connStr string) (*StubStore, error) {
 	if err != nil {
 		log.Fatal("Cannot connect to the database - ", err)
 	}
-	return &StubStore{db}, nil
+	return &Store{db}, nil
 }
 
-func (s *StubStore) Init(queries ...string) {
+func (s *Store) Init(queries ...string) {
 	for _, query := range queries {
-		_, err := s.db.Exec(query)
+		_, err := s.DB.Exec(query)
 		if err != nil {
 			log.Fatalf("Error executing '%s' : - %s", query, err)
 		}
 	}
 }
 
-func (s *StubStore) GetEventNameByID(id string) (name string) {
+func (s *Store) GetEventNameByID(id string) (name string) {
 	query := fmt.Sprintf("SELECT name FROM events WHERE id=%s;", id)
-	s.db.QueryRow(query).Scan(&name)
+	s.DB.QueryRow(query).Scan(&name)
 	return name
+}
+
+func (s *Store) PostEvent(name string) {
+	query := fmt.Sprintf("INSERT INTO events (name) VALUES ('%s');", name)
+	_, err := s.DB.Exec(query)
+	if err != nil {
+		log.Fatal("Could not insert into the database - ", err)
+	}
 }
