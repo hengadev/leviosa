21171a7cc005487cbe2e63e07cd13f4ed69571de
GaryHY <henry.gary@hotmail.com>
2024-05-28 11:38:51 +0200

    refacto: use of run function inside main instead

--- backend/cmd/event-reservation-app/main.go
+++ backend/cmd/event-reservation-app/main.go
@@ -1,28 +1,38 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"os"
+	"os/signal"
+
 	"github.com/GaryHY/event-reservation-app/internal/api"
-	"github.com/GaryHY/event-reservation-app/internal/database"
-	// "github.com/GaryHY/event-reservation-app/internal/somefunc"
+	"github.com/GaryHY/event-reservation-app/internal/database/s3"
+	"github.com/GaryHY/event-reservation-app/internal/database/sqlite"
+
+	// cron job import
+	"github.com/GaryHY/event-reservation-app/internal/cron"
 	"github.com/joho/godotenv"
 	// "github.com/robfig/cron"
 	"log"
 	"net/http"
 )
 
-func main() {
-	// load the env variables
+// TODO: Use that function when done with the app, and find a way to stop it using CTRL + C.
+// get the function run that I am going to use in main from this link : https://grafana.com/blog/2024/02/09/how-i-write-http-services-in-go-after-13-years/
+func run(ctx context.Context, w io.Writer, args []string) error {
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("error loading .env file")
+		return fmt.Errorf("Failed to load the environment variables - %w", err)
 	}
-
 	// create the store and server
 	store, err := sqlite.NewStore("db.sqlite")
-	photostore := sqlite.NewPhotoStore()
+	photostore := s3.NewPhotoStore()
 	if err != nil {
-		log.Fatal("Cannot connect to the database")
+		return fmt.Errorf("Cannot connect to the database - %w", err)
 	}
 	server := api.NewServer(store, photostore)
 
@@ -35,7 +45,34 @@ func main() {
 
 	fmt.Println("Running server on port 5000...")
 	if err := http.ListenAndServe(":5000", server); err != nil {
+		return fmt.Errorf("Cannot launch the server - %w", err)
+	}
+	return nil
+}
+func main() {
+	// TODO: Use that function when done with the app, and find a way to stop it using CTRL + C.
+	// ctx := context.Background()
+	// if err := run(ctx, os.Stdout, os.Args); err != nil {
+	// 	fmt.Fprintf(os.Stderr, "%s\n", err)
+	// 	os.Exit(1)
+	// }
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Failed to load the environment variables - ", err)
+	}
+	// create the store and server
+	store, err := sqlite.NewStore("db.sqlite")
+	photostore := s3.NewPhotoStore()
+	if err != nil {
+		log.Fatal("Cannot connect to the database - ", err)
+	}
+	server := api.NewServer(store, photostore)
+
+	cron.SetCron() // setting the cron job
+
+	fmt.Println("Running server on port 5000...")
+	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatal("Cannot launch the server - ", err)
 	}
-
 }
