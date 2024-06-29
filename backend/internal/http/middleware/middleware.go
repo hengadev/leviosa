51b4e6c7a572590362cd573652fc9586367b86dd
GaryHY <henry.gary@hotmail.com>
2024-06-29 09:31:46 +0200

    feat: add middleware structs to help

---
+++ backend/internal/http/middleware/middleware.go
@@ -0,0 +1,14 @@
+package middleware
+
+import "net/http"
+
+type Middleware func(http.Handler) http.Handler
+
+// to add a list of middleware together.
+func NewMiddleware(mws ...Middleware) http.Handler {
+	var h http.Handler
+	for _, mw := range mws {
+		h = mw(h)
+	}
+	return h
+}
