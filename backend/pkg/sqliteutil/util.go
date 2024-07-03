d2d37fa49f714faedc71147b359c69ea8bfeee9e
GaryHY <henry.gary@hotmail.com>
2024-07-03 13:54:36 +0200

    feat: add util function to write update query

--- backend/pkg/sqliteutil/util.go
+++ backend/pkg/sqliteutil/util.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"reflect"
+	"strings"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -35,4 +37,25 @@ func Init(db *sql.DB, queries ...string) error {
 		}
 	}
 	return nil
+}
+
+// Return the necessary elements to write a query update to target the non zero value.
+func WriteQueryUpdate[S any](s S) (string, []any) {
+	var tables []string
+	var values []any
+	v := reflect.ValueOf(s)
+	t := reflect.TypeOf(s)
+	vf := reflect.VisibleFields(t)
+	name := fmt.Sprintf("%ss", strings.ToLower(t.Name()))
+	query := fmt.Sprintf("UPDATE %s set ", name)
+	for _, f := range vf {
+		value := v.FieldByName(f.Name)
+		if !value.IsZero() && value.CanInterface() {
+			tables = append(tables, fmt.Sprintf("%s=?", strings.ToLower(f.Name)))
+			values = append(values, value.Interface())
+		}
+	}
+	query += strings.Join(tables, ", ")
+	query += " WHERE id=?;"
+	return query, values
 }
