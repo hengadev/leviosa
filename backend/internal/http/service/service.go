f29daa636edfee5aa04f44ce190ceeb96e11f5ce
GaryHY <henry.gary@hotmail.com>
2024-07-03 12:50:49 +0200

    fix: change pointer to interface to interface in struct field

--- backend/internal/http/service/service.go
+++ backend/internal/http/service/service.go
@@ -1,15 +1,10 @@
 package handler
 
 import (
-	// "net/http"
-
 	"github.com/GaryHY/event-reservation-app/internal/domain/session"
 	"github.com/GaryHY/event-reservation-app/internal/domain/user"
 )
 
-// in the grpc thing, it is our baseHandler in the grpc server struct
-
-// TODO: add all the services.
 type Service struct {
 	User    *user.Service
 	Session *session.Service
@@ -17,8 +12,8 @@ type Service struct {
 
 // TODO: add all the repos readers.
 type Repo struct {
-	User    *user.Reader
-	Session *session.Reader
+	User    user.Reader
+	Session session.Reader
 }
 
 type Services struct {
