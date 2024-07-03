976b75fd09628b59fcb54eb4e44390bba80951db
GaryHY <henry.gary@hotmail.com>
2024-07-03 13:47:35 +0200

    feat: add outline for updating user function

---
+++ backend/internal/domain/user/update_account.go
@@ -0,0 +1,19 @@
+package user
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/GaryHY/event-reservation-app/internal/domain"
+)
+
+func (s *Service) UpdateAccount(ctx context.Context, userCandidate *User) error {
+	if pbms := userCandidate.Validate(); len(pbms) > 0 {
+		return app.NewInvalidUserToUpdateErr(pbms)
+	}
+	// TODO: use a different function if the email needs to be modified.
+	if err := s.repo.ModifyAccount(ctx, userCandidate); err != nil {
+		return fmt.Errorf("add account: %w", err)
+	}
+	return nil
+}
