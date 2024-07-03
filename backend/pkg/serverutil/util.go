1072bedb68c96056d05114eaf0fff7572d08aa36
GaryHY <henry.gary@hotmail.com>
2024-07-03 13:54:59 +0200

    feat: add some helper server functions

--- backend/pkg/serverutil/util.go
+++ backend/pkg/serverutil/util.go
@@ -1,12 +1,16 @@
 package serverutil
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-// func Decode[T any](r io.Reader) (*T, error) {
+type Validator interface {
+	Valid(ctx context.Context) (problems map[string]string)
+}
+
 func Decode[T any](r *http.Request) (T, error) {
 	var res T
 	defer r.Body.Close()
@@ -16,11 +20,35 @@ func Decode[T any](r *http.Request) (T, error) {
 	return res, nil
 }
 
+func DecodeValid[T Validator](r *http.Request) (T, map[string]string, error) {
+	var v T
+	if v, err := Decode[T](r); err != nil {
+		return v, nil, fmt.Errorf("decode json: %w", err)
+	}
+	if pbms := v.Valid(r.Context()); len(pbms) > 0 {
+		return v, nil, fmt.Errorf("invalid %T: %d problems", v, len(pbms))
+	}
+	return v, nil, nil
+}
+
 func Encode[T any](w http.ResponseWriter, status int, v T) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	if err := json.NewEncoder(w).Encode(&v); err != nil {
 		return fmt.Errorf("encode json: %w", err)
+	}
+	return nil
+}
+
+func WriteResponse(w http.ResponseWriter, message string, status int) error {
+	resBody := struct {
+		Message string `json:"message"`
+	}{message}
+	// TODO: that gives an error for whatever reason
+	w.WriteHeader(status)
+	if err := json.NewEncoder(w).Encode(resBody); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return err
 	}
 	return nil
 }
