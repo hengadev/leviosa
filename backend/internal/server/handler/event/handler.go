393aba5cbe1c047400d4647bf4a2e073a19b184a
GaryHY <henry.gary@hotmail.com>
2024-07-19 21:46:58 +0200

    refacto: use of handler.Handler in event handler

--- backend/internal/server/handler/event/handler.go
+++ backend/internal/server/handler/event/handler.go
@@ -7,21 +7,30 @@ import (
 	"net/http"
 
 	"github.com/GaryHY/event-reservation-app/internal/domain/event"
-	"github.com/GaryHY/event-reservation-app/internal/http/handler"
-	mw "github.com/GaryHY/event-reservation-app/internal/http/middleware"
+	"github.com/GaryHY/event-reservation-app/internal/server/handler"
+	mw "github.com/GaryHY/event-reservation-app/internal/server/middleware"
+	"github.com/GaryHY/event-reservation-app/internal/server/service"
 	"github.com/GaryHY/event-reservation-app/pkg/serverutil"
 )
 
+type Handler struct {
+	*handler.Handler
+}
+
+func NewHandler(handler *handler.Handler) *Handler {
+	return &Handler{handler}
+}
+
 // handler
-func FindEventByID(eventRepo event.Reader) http.Handler {
+func (h *Handler) FindEventByID() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx, cancel := context.WithCancel(r.Context())
 		defer cancel()
 		eventID := r.PathValue("id")
-		event, err := eventRepo.GetEventByID(ctx, eventID)
+		event, err := h.Repos.Event.GetEventByID(ctx, eventID)
 		if err != nil {
 			slog.ErrorContext(ctx, "failed to get the user ID", "error", err)
-			http.Error(w, handler.NewBadRequestErr(err), http.StatusBadRequest)
+			http.Error(w, errsrv.NewBadRequestErr(err), http.StatusBadRequest)
 			return
 		}
 		if err := serverutil.Encode(w, http.StatusOK, event); err != nil {
@@ -32,7 +41,8 @@ func FindEventByID(eventRepo event.Reader) http.Handler {
 	})
 }
 
-func FindEventsForUser(eventRepo event.Reader) http.Handler {
+// func FindEventsForUser(eventRepo event.Reader) http.Handler {
+func (h *Handler) FindEventsForUser() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx, cancel := context.WithCancel(r.Context())
 		defer cancel()
@@ -40,22 +50,22 @@ func FindEventsForUser(eventRepo event.Reader) http.Handler {
 		if userID == "" { // TODO: find better thing to do to check if the userID stored is valid.
 			// some error with the auth that did not sent the userID in the context.
 		}
-		resBody, err := eventRepo.GetEventForUser(ctx, userID)
+		resBody, err := h.Repos.Event.GetEventForUser(ctx, userID)
 		if err != nil {
 			slog.ErrorContext(ctx, "failed to get the events for the user", "error", err)
-			http.Error(w, handler.NewInternalErr(err), http.StatusInternalServerError)
+			http.Error(w, errsrv.NewInternalErr(err), http.StatusInternalServerError)
 			return
 		}
 		if err := serverutil.Encode(w, http.StatusOK, resBody); err != nil {
 			slog.ErrorContext(ctx, "failed to send the user", "error", err)
-			http.Error(w, handler.NewInternalErr(err), http.StatusInternalServerError)
+			http.Error(w, errsrv.NewInternalErr(err), http.StatusInternalServerError)
 			return
 		}
 	})
 }
 
 // TODO: add the fact that creating an event, should also create a vote and a table with the style votes_month_year.
-func CreateEvent(svc *event.Service) http.Handler {
+func (h *Handler) CreateEvent() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx, cancel := context.WithCancel(r.Context())
 		defer cancel()
@@ -63,66 +73,66 @@ func CreateEvent(svc *event.Service) http.Handler {
 		event, err := serverutil.Decode[event.Event](r)
 		if err != nil {
 			slog.ErrorContext(ctx, "failed to decode the event", "error", err)
-			http.Error(w, handler.NewBadRequestErr(err), http.StatusBadRequest)
+			http.Error(w, errsrv.NewBadRequestErr(err), http.StatusBadRequest)
 			return
 		}
 		// use the service to make that event
-		eventID, err := svc.CreateEvent(ctx, &event)
+		eventID, err := h.Svcs.Event.CreateEvent(ctx, &event)
 		if err != nil {
 			slog.ErrorContext(ctx, "failed to create the event", "error", err)
-			http.Error(w, handler.NewInternalErr(err), http.StatusInternalServerError)
+			http.Error(w, errsrv.NewInternalErr(err), http.StatusInternalServerError)
 			return
 		}
 		// send the event id to the user
 		if err := serverutil.Encode(w, http.StatusOK, eventID); err != nil {
 			slog.ErrorContext(ctx, "failed to send the event", "error", err)
-			http.Error(w, handler.NewInternalErr(err), http.StatusInternalServerError)
+			http.Error(w, errsrv.NewInternalErr(err), http.StatusInternalServerError)
 			return
 		}
 	})
 }
 
-func DeleteEvent(svc *event.Service) http.Handler {
+func (h *Handler) DeleteEvent() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx, cancel := context.WithCancel(r.Context())
 		defer cancel()
 		// get the event id from the url path
 		eventID := r.PathValue("id")
 		// use the service to delete that event
-		resEventID, err := svc.RemoveEvent(ctx, eventID)
+		resEventID, err := h.Svcs.Event.RemoveEvent(ctx, eventID)
 		if err != nil {
 			slog.ErrorContext(ctx, "failed to delete the event", "error", err)
-			http.Error(w, handler.NewInternalErr(err), http.StatusInternalServerError)
+			http.Error(w, errsrv.NewInternalErr(err), http.StatusInternalServerError)
 			return
 		}
 		// send the event id to the user to specify that the event is deleted properly.
 		if err = serverutil.Encode(w, http.StatusInternalServerError, resEventID); err != nil {
 			slog.ErrorContext(ctx, "failed to send the event ID", "error", err)
-			http.Error(w, handler.NewInternalErr(err), http.StatusInternalServerError)
+			http.Error(w, errsrv.NewInternalErr(err), http.StatusInternalServerError)
 			return
 		}
 	})
 }
 
-func ModifyEvent(svc *event.Service) http.Handler {
+func (h *Handler) ModifyEvent() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx, cancel := context.WithCancel(r.Context())
 		defer cancel()
 		event, err := serverutil.Decode[event.Event](r)
 		if err != nil {
 			slog.ErrorContext(ctx, "failed to decode the event", "error", err)
-			http.Error(w, handler.NewBadRequestErr(err), http.StatusBadRequest)
+			http.Error(w, errsrv.NewBadRequestErr(err), http.StatusBadRequest)
 			return
 		}
-		eventID, err := svc.ModifyEvent(ctx, &event)
+		eventID, err := h.Svcs.Event.ModifyEvent(ctx, &event)
 		if err != nil {
 			slog.ErrorContext(ctx, "failed to update the event", "error", err)
-			http.Error(w, handler.NewInternalErr(err), http.StatusInternalServerError)
+			http.Error(w, errsrv.NewInternalErr(err), http.StatusInternalServerError)
 			return
 		}
 		if err = serverutil.Encode(w, http.StatusInternalServerError, eventID); err != nil {
 			slog.ErrorContext(ctx, "failed to send the event ID", "error", err)
-			http.Error(w, handler.NewInternalErr(err), http.StatusInternalServerError)
+			http.Error(w, errsrv.NewInternalErr(err), http.StatusInternalServerError)
 			return
 		}
 	})
