135f947993cf4561998dff0b48767733ee3fdd4e
GaryHY <henry.gary@hotmail.com>
2024-07-30 12:21:01 +0200

    chore: add shadcn-svelte configuration

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	function addValue() {}
+</script>
+
+<h1>Welcome to SvelteKit brother</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Button on:click={addValue}>Click me</Button>
+
+<style></style>
