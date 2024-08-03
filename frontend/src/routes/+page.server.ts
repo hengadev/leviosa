83dc96eaaf5ffdb82429c5e8efe5a3a49ac10920
GaryHY <henry.gary@hotmail.com>
2024-08-03 10:42:12 +0200

    fix: server side register function to sign in with cookie

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -17,15 +17,19 @@ function validate(email: string, password: string): boolean {
 }
 
 type CookieParsed = {
-    sessionId: string,
+    Name: string,
+    Value: string,
     Expires: Date,
     HttpOnly: boolean,
     Secure: boolean,
 }
 
+import { cookieName } from "$lib/types"
+
 function parseCookie(cookie: string): CookieParsed {
     const res: CookieParsed = {
-        sessionId: "",
+        Name: cookieName,
+        Value: "",
         Expires: new Date(),
         HttpOnly: true,
         Secure: true,
@@ -33,9 +37,11 @@ function parseCookie(cookie: string): CookieParsed {
     cookie.split(";").map((field) => {
         const split = field.trim().split("=");
         if (split[0] === "Expires") {
-            split[1] = new Date(split[1]);
+            res.Expires = new Date(split[1]);
         }
-        res[split[0]] = split[1] ?? true;
+        if (split[0] === cookieName) {
+            res.Value = split[1]
+        }
     });
     return res;
 }
@@ -45,23 +51,24 @@ export const register: Action = async ({ request, cookies }) => {
     const formData = await request.formData()
     const email = String(formData.get("email"))
     const password = String(formData.get("password"))
-    // validate the email and password
+    // validate the email and password client side
     validate(email, password)
     const body = JSON.stringify({ email, password })
     // make request to the server.
-    const res = await fetch("http://localhost:5000/signin", {
+    const res = await fetch("http://localhost:3500/api/v1/signin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body
     })
-    // TODO: Do better error handling between the server and this file to handle is the user exists for example.
     if (res.ok) {
         // parse the reponse to reform the cookie.
         const cookieParsed = parseCookie(res.headers.getSetCookie()[0])
-        cookies.set("sessionId", cookieParsed.sessionId, {
+        cookies.set(cookieName, cookieParsed.Value, {
             path: "/",
+            maxAge: 60 * 60 * 24 * 30,
+            ...cookieParsed,
         })
-        // redirect to the auth home page.
+        // redirect to sign in page.
         throw redirect(302, "/app")
     }
     // return data, in case we did not redirect the page and the fetching failed for some reason.
