a0d16ba58016e7bdf407ea591ce958ba0069cffb
GaryHY <henry.gary@hotmail.com>
2024-08-11 14:41:47 +0200

    test: add in memory sqlite database for testing

---
+++ backend/pkg/sqliteutil/testdatabase/testdatabase.go
@@ -0,0 +1,36 @@
+package testdatabase
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"os"
+
+	util "github.com/GaryHY/event-reservation-app/pkg/sqliteutil"
+	"github.com/pressly/goose/v3"
+)
+
+func NewDatabase(ctx context.Context) (*sql.DB, error) {
+	db, err := util.Connect(ctx, ":memory:")
+	if err != nil {
+		return nil, fmt.Errorf("create new in memory database: %w", err)
+	}
+	return db, nil
+}
+
+func Teardown(db *sql.DB) error {
+	return db.Close()
+}
+
+func Setup(ctx context.Context, db *sql.DB, version int64) error {
+	// setup goose
+	goose.SetBaseFS(nil)
+	// Set the dialect to SQLite3
+	if err := goose.SetDialect("sqlite3"); err != nil {
+		return fmt.Errorf("Failed to set dialect: %w", err)
+	}
+	if err := goose.UpToContext(ctx, db, os.Getenv("TEST_MIGRATION_PATH"), version); err != nil {
+		return fmt.Errorf("test migration up failed with version %d: %w", version, err)
+	}
+	return nil
+}
