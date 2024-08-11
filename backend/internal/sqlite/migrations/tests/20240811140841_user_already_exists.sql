fc481ab28f08279e76fdc589d74391409f7c904b
GaryHY <henry.gary@hotmail.com>
2024-08-11 20:26:22 +0200

    feat: add migrations for AddAccount user sqlite implementaion

---
+++ backend/internal/sqlite/migrations/tests/20240811140841_user_already_exists.sql
@@ -0,0 +1,41 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
+INSERT INTO users (
+    email,
+    password,
+    createdat,
+    loggedinat,
+    role,
+    birthdate,
+    lastname,
+    firstname,
+    gender,
+    telephone,
+    address,
+    city,
+    postalcard
+    ) VALUES (
+    'john.doe@gmail.com',
+    '$a9rfNhA$N$A78#m',
+    '',
+    '',
+    'basic',
+    '1998-07-12',
+    'DOE',
+    'John',
+    'M',
+    '0123456789',
+    'Impasse Inconnue',
+    'Paris',
+    12345
+    );
+-- NOTE: do not care about the field createdat and loggedinat
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+DELETE from users ORDER BY id DESC LIMIT 1;
+-- +goose StatementEnd
