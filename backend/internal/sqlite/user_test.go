3b96562f901aab0eb5c2e24ee0ff50492d7c0af6
GaryHY <henry.gary@hotmail.com>
2024-08-11 22:27:57 +0200

    test: add AddAccount test case for SQLite user domain repository implementation

---
+++ backend/internal/sqlite/user_test.go
@@ -0,0 +1,52 @@
+package sqlite_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/GaryHY/event-reservation-app/internal/domain/user"
+	testdb "github.com/GaryHY/event-reservation-app/pkg/sqliteutil/testdatabase"
+	"github.com/GaryHY/event-reservation-app/tests"
+)
+
+func TestAddAccount(t *testing.T) {
+	// NOTE: done cases:
+	// - add the user, right case
+	// - user already exists so can not add the user
+	// TODO: cases:
+	// - email unique
+	// - nom prenom unique
+	// - telephone unique
+	t.Setenv("TEST_MIGRATION_PATH", "./migrations/tests")
+	tests := []struct {
+		usr            *user.User
+		wantErr        bool
+		expectedUserID int
+		version        int64
+		name           string
+	}{
+		{usr: johndoe, expectedUserID: 1, wantErr: false, version: 20240811085134, name: "add the user"},
+		{usr: johndoe, expectedUserID: 0, wantErr: true, version: 20240811140841, name: "user already exists"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			repo, err := setupUserRepo(ctx, tt.version)
+			if err != nil {
+				t.Errorf("setup repo: %s", err)
+			}
+			userID, err := repo.AddAccount(ctx, tt.usr)
+			test.Assert(t, userID, tt.expectedUserID)
+			test.Assert(t, err != nil, tt.wantErr)
+			if tt.wantErr == false {
+				userFromDB, err := getOnlyUser(ctx, repo.DB)
+				if err != nil {
+					t.Errorf("user not found after addition to database: %s", err)
+				}
+				// TODO: see if the users are the same ?
+				_ = userFromDB
+			}
+			testdb.Teardown(repo.DB)
+		})
+	}
+}
