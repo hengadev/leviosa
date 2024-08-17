7cab4addff72a91847dec45cd0b948f0aa939b37
GaryHY <henry.gary@hotmail.com>
2024-08-17 20:34:46 +0200

    feat: add test configuration for vitest and playwright

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,28 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import type { Plugin } from 'vite';
+
+const add_browser_onmount: Plugin = {
+    name: 'vite-plugin-onmount',
+    config(config) {
+        if (process.env.VITEST) {
+            if (!config.resolve?.conditions) {
+                if (!config.resolve) {
+                    config.resolve = {};
+                }
+                config.resolve.conditions = [];
+            }
+            config.resolve.conditions.unshift('browser');
+        }
+    }
+};
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+    plugins: [sveltekit(), add_browser_onmount],
+    test: {
+        include: ['src/**/*.{test,spec}.{js,ts}', 'tests/**/*.{test,spec}.{js,ts}'],
+        environment: 'jsdom',
+        setupFiles: ['src/vitest.setup.ts'],
+        globals: true,
+    }
 });
