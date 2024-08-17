b810496bfbb4eafd665ddc6f880f85b40245fd01
GaryHY <henry.gary@hotmail.com>
2024-08-17 23:17:45 +0200

    feat: add backend Valid function for Vote domain

--- backend/internal/domain/vote/vote.go
+++ backend/internal/domain/vote/vote.go
@@ -1,6 +1,10 @@
 package vote
 
-import "github.com/google/uuid"
+import (
+	"context"
+	"reflect"
+	"time"
+)
 
 const VoteSeparator = "|"
 
@@ -22,3 +26,40 @@ func NewVote(day, month, year int) *Vote {
 // NOTE: the way I am going to make the tables
 // vote : month year days
 // vote_august_2024 : userID days
+
+func (v Vote) Valid(ctx context.Context) map[string]string {
+	var pbms = make(map[string]string)
+	vf := reflect.VisibleFields(reflect.TypeOf(v))
+	now := time.Now().UTC()
+	for _, f := range vf {
+		switch f.Name {
+		case "Day":
+			if int(now.Month())%2 == 0 && int(now.Month()) != 8 && v.Day > 30 {
+				pbms["not_enough_day"] = "this month has 30 days"
+			}
+			if v.Day < 1 {
+				pbms["day_too_small"] = "should be > 1"
+			}
+			if v.Day > 31 {
+				pbms["day_too_large"] = "should be < 32"
+			}
+		case "Month":
+			if v.Month <= int(now.Month()) && v.Year == now.Year() {
+				pbms["past_month"] = "date should not be in the past"
+			}
+			if v.Month < 1 {
+				pbms["month_too_small"] = "should be > 1"
+			}
+			if v.Month > 12 {
+				pbms["month_too_large"] = "should be < 13"
+			}
+		case "Year":
+			if v.Year < now.Year() {
+				pbms["year"] = "should be > than current year"
+			}
+		default:
+			continue
+		}
+	}
+	return pbms
+}
