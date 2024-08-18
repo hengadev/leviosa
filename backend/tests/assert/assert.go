e1e95576d9971883510ce2a239f9577387320450
GaryHY <henry.gary@hotmail.com>
2024-08-18 17:22:40 +0200

    test: add assert package DeepEqual and NotDeepEqual functions

--- backend/tests/assert/assert.go
+++ backend/tests/assert/assert.go
@@ -1,6 +1,7 @@
 package assert
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -18,6 +19,20 @@ func Equal[T comparable](t testing.TB, got, want T) {
 func NotEqual[T comparable](t testing.TB, got, want T) {
 	t.Helper()
 	if got == want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func ReflectEqual[T any](t testing.TB, got, want T) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func ReflectNotEqual[T any](t testing.TB, got, want T) {
+	t.Helper()
+	if reflect.DeepEqual(got, want) {
 		t.Errorf("got %v, want %v", got, want)
 	}
 }
