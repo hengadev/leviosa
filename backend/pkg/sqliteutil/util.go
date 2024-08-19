17ddd69abe09bb7bf8b68edd732d9c204003953d
GaryHY <henry.gary@hotmail.com>
2024-08-19 18:33:21 +0200

    fix(sqliteutil): add WHERE clause and prohibited field support to WriteUpdateQuery

--- backend/pkg/sqliteutil/util.go
+++ backend/pkg/sqliteutil/util.go
@@ -33,25 +33,56 @@ func HashPassword(password string) (string, error) {
 	return string(bytes), nil
 }
 
+// Helper function to check if a struct field belongs to a list of strings provided.
+func isProhibitedField(name string, prohibitedFields ...string) error {
+	for _, prohibitedField := range prohibitedFields {
+		if name == prohibitedField {
+			return fmt.Errorf("field %s prohibited", prohibitedField)
+		}
+	}
+	return nil
+}
+
 // Return the necessary elements to write a query update to target the non zero value.
-func WriteUpdateQuery[S any](s S) (string, []any) {
+func WriteUpdateQuery[T any](
+	object T,
+	whereMap map[string]any,
+	prohibitedFields ...string,
+) (string, []any, error) {
+	fail := func(err error) (string, []any, error) {
+		return "", nil, fmt.Errorf("write update query, %w", err)
+	}
 	var tables []string
 	var values []any
-	v := reflect.ValueOf(s)
-	t := reflect.TypeOf(s)
+	v := reflect.ValueOf(object)
+	t := reflect.TypeOf(object)
 	vf := reflect.VisibleFields(t)
 	tableName := fmt.Sprintf("%ss", strings.ToLower(t.Name()))
 	query := fmt.Sprintf("UPDATE %s set ", tableName)
 	for _, f := range vf {
 		value := v.FieldByName(f.Name)
 		if !value.IsZero() && value.CanInterface() {
-			tables = append(tables, fmt.Sprintf("%s=?", strings.ToLower(f.Name)))
+			if err := isProhibitedField(f.Name, prohibitedFields...); err != nil {
+				return fail(err)
+			}
+			tables = append(tables, placeholder(strings.ToLower(f.Name)))
 			values = append(values, value.Interface())
 		}
 	}
 	query += strings.Join(tables, ", ")
-	query += " WHERE id=?;"
-	return query, values
+	query += " WHERE "
+	var wherePlaceholder []string
+	for key, value := range whereMap {
+		minKey := strings.ToLower(key)
+		wherePlaceholder = append(wherePlaceholder, placeholder(minKey))
+		values = append(values, value)
+	}
+	query += strings.Join(wherePlaceholder, " AND ") + ";"
+	return query, values, nil
+}
+
+func placeholder(name string) string {
+	return fmt.Sprintf("%s=?", name)
 }
 
 // Return the necessary elements to write a query update to target the non zero value.
