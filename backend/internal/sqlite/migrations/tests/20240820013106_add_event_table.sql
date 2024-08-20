cda54271e82520d91ff846ee945ff91fdb09526c
GaryHY <henry.gary@hotmail.com>
2024-08-20 14:03:52 +0200

    test: enforce positive or null constraint on event.freeplace test migration

--- backend/internal/sqlite/migrations/tests/20240820013106_add_event_table.sql
+++ backend/internal/sqlite/migrations/tests/20240820013106_add_event_table.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS events (
     id TEXT NOT NULL PRIMARY KEY,
     location TEXT NOT NULL,        
     placecount INTEGER NOT NULL,
-    freeplace INTEGER NOT NULL,
+    freeplace INTEGER CHECK(freeplace >= 0),
     beginat TEXT NOT NULL,
     sessionduration INTEGER NOT NULL,
     priceid TEXT NOT NULL UNIQUE,
