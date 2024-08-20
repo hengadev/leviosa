383111b558dbb4f73c1b4f349697ae9bd94830fc
GaryHY <henry.gary@hotmail.com>
2024-08-20 23:30:42 +0200

    test: add RemoveEvent test case for SQLite event domain repository implementation

---
+++ backend/internal/sqlite/event/remove_event_test.go
@@ -0,0 +1,43 @@
+package eventRepository_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/GaryHY/event-reservation-app/internal/sqlite/event"
+	testdb "github.com/GaryHY/event-reservation-app/pkg/sqliteutil/testdatabase"
+	test "github.com/GaryHY/event-reservation-app/tests"
+	"github.com/GaryHY/event-reservation-app/tests/assert"
+)
+
+func TestRemoveEvent(t *testing.T) {
+	t.Setenv("TEST_MIGRATION_PATH", "../migrations/tests")
+
+	randEventID := test.GenerateRandomString(16)
+
+	tests := []struct {
+		eventID         string
+		expectedEventID string
+		wantErr         bool
+		version         int64
+		name            string
+	}{
+		{eventID: baseEventWithPriceID.ID, expectedEventID: "", wantErr: true, version: 20240820013106, name: "no event in database"},
+		{eventID: randEventID, expectedEventID: "", wantErr: true, version: 20240820023513, name: "ID do not exist in database"},
+		{eventID: baseEventWithPriceID.ID, expectedEventID: baseEventWithPriceID.ID, wantErr: false, version: 20240820023513, name: "nominal case"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			ctx := context.Background()
+			repo, err := testdb.SetupRepo(ctx, tt.version, eventRepository.New)
+			defer testdb.Teardown(repo.DB)
+			if err != nil {
+				t.Errorf("setup event domain stub repository: %s", err)
+			}
+			eventID, err := repo.RemoveEvent(ctx, tt.eventID)
+			assert.Equal(t, err != nil, tt.wantErr)
+			assert.Equal(t, eventID, tt.expectedEventID)
+		})
+	}
+}
