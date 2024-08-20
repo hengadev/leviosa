07e3f8cdc5dcb1941d4ebfaf8752679efe19870a
GaryHY <henry.gary@hotmail.com>
2024-08-20 13:05:14 +0200

    fix: GetAllEvents for SQLite event domain repository implementation

--- backend/internal/sqlite/event.go
+++ backend/internal/sqlite/event.go
@@ -79,30 +79,35 @@ func (e *EventRepository) GetEventByUserID(ctx context.Context, userID string) (
 }
 
 func (e *EventRepository) GetAllEvents(ctx context.Context) ([]*event.Event, error) {
-	events := make([]*event.Event, 0)
-	rows, err := e.DB.QueryContext(ctx, "SELECT * FROM events;")
+	var events []*event.Event
+	query := "SELECT id, location, placecount, freeplace, beginat, sessionduration, day, month, year FROM events;"
+
+	rows, err := e.DB.QueryContext(ctx, query)
 	if err != nil {
 		return nil, rp.NewErrRow(err)
 	}
 	defer rows.Close()
-	var dateTemp string
-
 	for rows.Next() {
+		var beginat string
+		var minutes int
 		event := &event.Event{}
 		if err := rows.Scan(
 			&event.ID,
 			&event.Location,
 			&event.PlaceCount,
-			&dateTemp,
-			&event.PriceID,
+			&event.FreePlace,
+			&beginat,
+			&minutes,
+			&event.Day,
+			&event.Month,
+			&event.Year,
 		); err != nil {
 			return nil, rp.NewErrScan(err)
 		}
-		// NOTE: Old version but now I am using another formatting to send to the frontend
-		// event.BeginAt, err = time.Parse(event.EventFormat, dateTemp)
-		event.BeginAt, err = time.Parse(time.RFC3339, dateTemp)
+		event.SessionDuration = time.Minute * time.Duration(minutes)
+		event.BeginAt, err = parseBeginAt(beginat, event.Day, event.Month, event.Year)
 		if err != nil {
-			return nil, fmt.Errorf("%s: %w", "error parsing time", err)
+			return nil, fmt.Errorf("parsing time: %w", err)
 		}
 		events = append(events, event)
 	}
