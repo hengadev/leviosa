18df8708d067177e31ca71fb4299f3d70cb91ec9
GaryHY <henry.gary@hotmail.com>
2024-08-21 20:58:06 +0200

    test: add GetSession test case for session domain

---
+++ backend/internal/domain/session/get_session_by_id_test.go
@@ -0,0 +1,38 @@
+package session_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/GaryHY/event-reservation-app/internal/domain/session"
+	test "github.com/GaryHY/event-reservation-app/tests"
+	"github.com/GaryHY/event-reservation-app/tests/assert"
+)
+
+func TestGetSession(t *testing.T) {
+	// TEST:
+	tests := []struct {
+		sessionID       string
+		initMap         KVMap
+		expectedSession *session.Session
+		wantErr         bool
+		name            string
+	}{
+		{sessionID: baseSession.ID, initMap: nil, expectedSession: nil, wantErr: true, name: "no session in database"},
+		{sessionID: test.GenerateRandomString(12), initMap: initMap, expectedSession: nil, wantErr: true, name: "id not in database"},
+		{sessionID: baseSession.ID, initMap: initMap, expectedSession: baseSession, wantErr: false, name: "nominal case"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			ctx := context.Background()
+			repo := NewStubSessionRepository(ctx, tt.initMap)
+			service := session.NewService(repo)
+			sess, err := service.GetSession(ctx, tt.sessionID)
+			assert.Equal(t, err != nil, tt.wantErr)
+			assert.ReflectEqual(t, sess, tt.expectedSession)
+
+		})
+	}
+}
