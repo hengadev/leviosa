679c31c5273b89d40dc878c3fd14ac4da6ea75b4
GaryHY <henry.gary@hotmail.com>
2024-08-24 15:20:31 +0200

    test: add testutil SetupSession to get sessionService, sessionRepo and teardown function

--- backend/pkg/testutil/handler.go
+++ backend/pkg/testutil/handler.go
@@ -1,75 +1,50 @@
 package testutil
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"testing"
 
 	"github.com/GaryHY/event-reservation-app/internal/domain/session"
 	"github.com/GaryHY/event-reservation-app/internal/domain/user"
-	"github.com/GaryHY/event-reservation-app/internal/redis/session"
-	// "github.com/GaryHY/event-reservation-app/internal/server/handler/session"
 	"github.com/GaryHY/event-reservation-app/internal/redis"
+	"github.com/GaryHY/event-reservation-app/internal/redis/session"
 	"github.com/GaryHY/event-reservation-app/internal/sqlite/user"
-	redistest "github.com/GaryHY/event-reservation-app/pkg/redisutil/testdatabase"
-	sqlitetest "github.com/GaryHY/event-reservation-app/pkg/sqliteutil/testdatabase"
+	testdb "github.com/GaryHY/event-reservation-app/pkg/sqliteutil/testdatabase"
 )
 
-// func SetupUser(t testing.TB, ctx context.Context, version int64) *userHandler.Handler {
 func SetupUser(t testing.TB, ctx context.Context, version int64) (*userService.Service, *userRepository.Repository) {
 	t.Helper()
-	sqlitedb, err := sqlitetest.NewDatabase(ctx)
+	db, err := testdb.NewDatabase(ctx)
 	if err != nil {
 		t.Error(err)
 	}
-	if err := sqlitetest.Setup(ctx, sqlitedb, version); err != nil {
+	if err := testdb.Setup(ctx, db, version); err != nil {
 		t.Error(err)
 	}
-	userRepo := userRepository.New(ctx, sqlitedb)
+	userRepo := userRepository.New(ctx, db)
 	userService := userService.New(userRepo)
 	return userService, userRepo
 }
 
-// func SetupSession(t testing.TB, ctx context.Context, version int64) *userHandler.Handler {
 type redisTeardownFunc func()
 
-func SetupSession(t testing.TB, ctx context.Context, version int64) (*sessionService.Service, *sessionRepository.Repository, redisTeardownFunc) {
-	t.Helper()
-	redisdb, err := redistest.NewTestDatabase(ctx)
-	if err != nil {
-		t.Error(err)
-	}
-
-	sessionRepo := sessionRepository.New(ctx, redisdb.DB)
-	sessionService := sessionService.New(sessionRepo)
-
-	teardown := func() {
-		redisdb.TearDown()
-	}
-	return sessionService, sessionRepo, teardown
-}
-
-type otherRedisTeardownFunc func() error
-
-// NOTE: the version of the setup that uses miniredis
-func OtherSetupSession(t testing.TB, ctx context.Context, initMap miniredis.InitMap[*sessionService.Values]) (*sessionService.Service, *sessionRepository.Repository, redisTeardownFunc) {
+func SetupSession(t testing.TB, ctx context.Context, initMap miniredis.InitMap[*sessionService.Values]) (*sessionService.Service, *sessionRepository.Repository, redisTeardownFunc) {
 	t.Helper()
 	client, err := miniredis.Setup(t, ctx)
 	if err != nil {
 		t.Errorf("setup miniredis: %s", err)
 	}
-
 	if err := miniredis.Init(t, ctx, client, initMap); err != nil {
 		t.Errorf("init miniredis: %s", err)
 	}
-
 	sessionRepo := sessionRepository.New(ctx, client)
 	sessionService := sessionService.New(sessionRepo)
-
 	teardown := func() {
 		if err := client.Close(); err != nil {
 			t.Errorf("closing miniredis: %s", err)
 		}
 	}
-
 	return sessionService, sessionRepo, teardown
 }
