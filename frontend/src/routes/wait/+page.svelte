9602d8cab44ccdfcf3c26072e610df15c5c8e1ea
GaryHY <henry.gary@hotmail.com>
2024-08-27 12:55:48 +0200

    feat: add the beginning of a page wait page

---
+++ frontend/src/routes/wait/+page.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+</script>
+
+<div class="container">
+	<!-- <div class="content"> -->
+	<h1 class="title">Coming soon...</h1>
+	<h2 class="subtitle">
+		SereniMeet Team are working very hard to bring something amazing. Stay tuned!
+	</h2>
+	<!-- </div> -->
+</div>
+
+<style>
+	.container {
+		display: grid;
+		place-content: center;
+		padding: 2rem;
+		height: 100vh;
+		width: 90%;
+	}
+
+	/* .content { */
+	/* 	background-color: #202127; */
+	/* 	padding: 4rem; */
+	/* 	padding-block: 3rem; */
+	/* 	border-radius: 0.5rem; */
+	/* 	outline: 1px solid #bdbdbd; */
+	/* 	box-shadow: */
+	/* 		rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, */
+	/* 		rgba(27, 31, 35, 0.15) 0px 0px 0px 1px; */
+	/* } */
+
+	.title,
+	.subtitle {
+		text-align: center;
+	}
+
+	.title {
+		color: #f67373;
+		font-size: clamp(1.15rem, 4vw, 2rem);
+		font-weight: 800;
+	}
+	.subtitle {
+		color: #3c3c43;
+		color: rgba(255, 255, 245, 0.86);
+		margin-top: 1rem;
+		font-size: clamp(1rem, 1.5vw, 1.5rem);
+	}
+
+	::placeholder {
+		font-weight: 300;
+	}
+</style>
