56784bed88855ff3f43619d7dbc43086ac41c531
GaryHY <henry.gary@hotmail.com>
2024-09-13 09:14:30 +0200

    feat: change user domain repository implementation FindAccountByID and AddAccount to fit using OAuth users

--- backend/internal/sqlite/user/add_account.go
+++ backend/internal/sqlite/user/add_account.go
@@ -2,25 +2,95 @@ package userRepository
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/GaryHY/event-reservation-app/internal/domain/user"
 	rp "github.com/GaryHY/event-reservation-app/internal/repository"
 	"github.com/GaryHY/event-reservation-app/pkg/sqliteutil"
 )
 
-func (u *Repository) AddAccount(ctx context.Context, usr *userService.User) (int64, error) {
-	hashpassword, err := sqliteutil.HashPassword(usr.Password)
+func (u *Repository) AddAccount(ctx context.Context, usr *userService.User, provider ...string) (int64, error) {
+	tx, err := u.DB.BeginTx(ctx, &sql.TxOptions{})
+	// TODO: better handling for the rollback case
+	defer func() {
+		if err := tx.Rollback(); err != nil && errors.Is(err, sql.ErrTxDone) {
+			// do some log thing if need be
+		}
+	}()
 	if err != nil {
-		return 0, err
+		return 0, fmt.Errorf("start transaction")
 	}
-	res, err := u.DB.ExecContext(ctx, "INSERT INTO users (email, password, createdat, loggedinat, role, lastname, firstname, gender, birthdate, telephone, address, city, postalcard) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", usr.Email, hashpassword, usr.CreatedAt, usr.LoggedInAt, usr.Role, usr.LastName, usr.FirstName, usr.Gender, usr.BirthDate, usr.Telephone, usr.Address, usr.City, usr.PostalCard)
+	var nullString sql.NullString
+	if usr.Password != "" {
+		hashpassword, err := sqliteutil.HashPassword(usr.Password)
+		if err != nil {
+			return 0, rp.NewBadQueryErr(err)
+		}
+		nullString.Valid = true //NOTE: Do I need that ?j
+		nullString.String = hashpassword
+	}
+
+	var providers string
+	// if err := u.DB.QueryRowContext(ctx, "SELECT oauth_providers from users where email = ?;", usr.Email).Scan(providers); err != sql.ErrNoRows {
+	if err := tx.QueryRowContext(ctx, "SELECT oauth_providers from users where email = ?;", usr.Email).Scan(providers); err != sql.ErrNoRows {
+		return 0, rp.NewNotFoundError(err)
+	}
+	providerList := parseProviders(providers)
+	addIfNotExist(providerList, usr.OAuthProvider)
+
+	query := "INSERT INTO users (email, password, createdat, loggedinat, role, lastname, firstname, gender, birthdate, telephone, address, city, postalcard, oauth_providers, oauth_ids) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
+	// res, err := u.DB.ExecContext(
+	res, err := tx.ExecContext(
+		ctx,
+		query,
+		usr.Email,
+		nullString,
+		usr.CreatedAt,
+		usr.LoggedInAt,
+		usr.Role,
+		usr.LastName,
+		usr.FirstName,
+		usr.Gender,
+		usr.BirthDate,
+		usr.Telephone,
+		usr.Address,
+		usr.City,
+		usr.PostalCard,
+		formatProvider(providerList),
+		usr.OAuthID,
+	)
 	if err != nil {
 		return 0, rp.NewRessourceCreationErr(err)
 	}
+	if err := tx.Commit(); err != nil {
+		return 0, fmt.Errorf("commit: %w", err)
+	}
 	lastInsertID, err := res.LastInsertId()
 	if lastInsertID == 0 {
-		return 0, fmt.Errorf("no user added")
+		return 0, rp.NewRessourceCreationErr(fmt.Errorf("no user added"))
 	}
 	return lastInsertID, nil
+}
+
+// function that given a list of string providers returns a formatted string of providers "providerA | providerB | providerC".
+func formatProvider(providers []string) string {
+	return strings.Join(providers, " | ")
+}
+
+// function that given a formatted list of providers "providerA | providerB | providerC" returns a list of providers
+func parseProviders(providers string) []string {
+	return strings.Split(providers, " | ")
+}
+
+func addIfNotExist(providers []string, provider string) []string {
+	for _, p := range providers {
+		if p == provider {
+			return providers
+		}
+	}
+	providers = append(providers, provider)
+	return providers
 }
--- backend/internal/sqlite/user/find_account_by_id.go
+++ backend/internal/sqlite/user/find_account_by_id.go
@@ -2,18 +2,19 @@ package userRepository
 
 import (
 	"context"
+	"database/sql"
 
 	"github.com/GaryHY/event-reservation-app/internal/domain/user"
 	rp "github.com/GaryHY/event-reservation-app/internal/repository"
 )
 
-// reader
 func (u *Repository) FindAccountByID(ctx context.Context, id int) (*userService.User, error) {
+	var nullString sql.NullString
 	var user userService.User
 	if err := u.DB.QueryRowContext(ctx, "SELECT * FROM users WHERE id = ?;", id).Scan(
 		&user.ID,
 		&user.Email,
-		&user.Password,
+		&nullString,
 		&user.CreatedAt,
 		&user.LoggedInAt,
 		&user.Role,
@@ -25,8 +26,14 @@ func (u *Repository) FindAccountByID(ctx context.Context, id int) (*userService.
 		&user.Address,
 		&user.City,
 		&user.PostalCard,
+		&user.OAuthProvider,
+		&user.OAuthID,
 	); err != nil {
 		return nil, rp.NewNotFoundError(err)
+	}
+	// get the passowrd in the user instance if not null
+	if nullString.Valid {
+		user.Password = nullString.String
 	}
 	return &user, nil
 }
