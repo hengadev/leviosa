445ced5d04b943a67d0fe29f328eb53eb6c3dec6
GaryHY <henry.gary@hotmail.com>
2024-12-28 21:36:18 +0100

    feat: add migration for adding pending_users table

---
+++ backend/internal/repository/sqlite/migrations/20241228203428_add_pending_users_table.sql
@@ -0,0 +1,22 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS pending_users (
+    email TEXT PRIMARY KEY,
+    password TEXT,
+	birthdate  TEXT NOT NULL,
+    lastname TEXT NOT NULL,
+    firstname TEXT NOT NULL,
+	gender TEXT NOT NULL,
+	telephone TEXT NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    postal_code TEXT NOT NULL UNIQUE, 
+    city TEXT NOT NULL UNIQUE, 
+    address1 TEXT NOT NULL UNIQUE, 
+    address2 TEXT NOT NULL UNIQUE 
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE pending_users;
+-- +goose StatementEnd
