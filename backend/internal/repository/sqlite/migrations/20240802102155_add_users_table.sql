8fa89bb0aa74f2833efd49e1ecdb2561e6ca7203
GaryHY <henry.gary@hotmail.com>
2024-12-28 22:50:26 +0100

    feat: add addresses fields in users table migration

--- backend/internal/repository/sqlite/migrations/20240802102155_add_users_table.sql
+++ backend/internal/repository/sqlite/migrations/20240802102155_add_users_table.sql
@@ -12,6 +12,10 @@ CREATE TABLE IF NOT EXISTS users (
     firstname TEXT NOT NULL,
 	gender TEXT NOT NULL CHECK (GENDER IN ("M", "F", "NB", "NP")),
 	telephone TEXT NOT NULL UNIQUE,
+    postal_code TEXT NOT NULL UNIQUE, 
+    city TEXT NOT NULL UNIQUE, 
+    address1 TEXT NOT NULL UNIQUE, 
+    address2 TEXT NOT NULL UNIQUE,
     google_id TEXT,
     apple_id TEXT,
     UNIQUE(lastname, firstname)
