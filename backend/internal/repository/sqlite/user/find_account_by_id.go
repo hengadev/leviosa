d9651ea84e8897f8a288be1aab5ca683d57f1b02
GaryHY <henry.gary@hotmail.com>
2025-01-07 11:22:57 +0100

    feat: add FindAccountByID userRepository function

--- backend/internal/repository/sqlite/user/find_account_by_id.go
+++ backend/internal/repository/sqlite/user/find_account_by_id.go
@@ -3,34 +3,52 @@ package userRepository
 import (
 	"context"
 	"database/sql"
+	"errors"
 
 	"github.com/GaryHY/event-reservation-app/internal/domain/user/models"
 	rp "github.com/GaryHY/event-reservation-app/internal/repository"
 )
 
 func (u *Repository) FindAccountByID(ctx context.Context, id string) (*models.User, error) {
-	var nullPassword sql.NullString
-	var user models.User
-	if err := u.DB.QueryRowContext(ctx, "SELECT * FROM users WHERE id = ?;", id).Scan(
-		&user.ID,
+	var user *models.User
+	query := `
+        SELECT 
+            email,
+            picture,
+            role,
+            lastname,
+            firstname,
+            gender,
+            birthdate,
+            telephone,
+            postal_code,
+            city,
+            address1,
+            address2
+        FROM users
+        WHERE id = ?;`
+	if err := u.DB.QueryRowContext(ctx, query, id).Scan(
 		&user.EmailHash,
-		&nullPassword,
-		&user.CreatedAt,
-		&user.LoggedInAt,
+		&user.Picture,
 		&user.Role,
-		&user.BirthDate,
 		&user.LastName,
 		&user.FirstName,
 		&user.Gender,
+		&user.BirthDate,
 		&user.Telephone,
-		&user.GoogleID,
-		&user.AppleID,
+		&user.PostalCode,
+		&user.City,
+		&user.Address1,
+		&user.Address2,
 	); err != nil {
-		return nil, rp.NewNotFoundErr(err, "user by ID")
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return &models.User{}, rp.NewNotFoundErr(err, "unverified user")
+		case errors.Is(err, context.DeadlineExceeded), errors.Is(err, context.Canceled):
+			return nil, rp.NewContextErr(err)
+		default:
+			return nil, rp.NewDatabaseErr(err)
+		}
 	}
-	// get the passowrd in the user instance if not null
-	if nullPassword.Valid {
-		user.PasswordHash = nullPassword.String
-	}
-	return &user, nil
+	return user, nil
 }
