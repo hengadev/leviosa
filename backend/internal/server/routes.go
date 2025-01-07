efce273898c7dab059e2ff62f57ae3ccb7d115fc
GaryHY <henry.gary@hotmail.com>
2025-01-07 17:23:32 +0100

    feat: change 'mux' in routes definition file to be 'router'

--- backend/internal/server/routes.go
+++ backend/internal/server/routes.go
@@ -4,78 +4,100 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/GaryHY/event-reservation-app/internal/server/handler/checkout"
-	"github.com/GaryHY/event-reservation-app/internal/server/handler/event"
-	"github.com/GaryHY/event-reservation-app/internal/server/handler/payment"
-	// "github.com/GaryHY/event-reservation-app/internal/server/handler/register"
 	"github.com/GaryHY/event-reservation-app/internal/server/app"
+	"github.com/GaryHY/event-reservation-app/internal/server/handler/event"
+	"github.com/GaryHY/event-reservation-app/internal/server/handler/product"
 	"github.com/GaryHY/event-reservation-app/internal/server/handler/user"
 	"github.com/GaryHY/event-reservation-app/internal/server/handler/vote"
 	mw "github.com/GaryHY/event-reservation-app/internal/server/middleware"
-	"github.com/GaryHY/event-reservation-app/pkg/contextutil"
-	"github.com/GaryHY/event-reservation-app/pkg/serverutil"
 )
 
-// make all the routes start with "/api/v1"
 func (s *Server) addRoutes(h *app.App) {
-	mux := http.NewServeMux()
+	router := http.NewServeMux()
+
 	// basic route to test things out
-	mux.Handle("GET /api/v1/hello", sayHello(h))
+	router.HandleFunc("GET /api/v1/hello", sayHello)
 
-	// TODO: placed here just to test things out brother
-	// use some sort of middleware to handle that brother
-
-	// handler declaration
+	// handlers declaration
 	userHandler := userHandler.New(h)
 	voteHandler := vote.NewHandler(h)
-	paymentHandler := payment.NewHandler(h)
-	checkoutHandler := checkout.NewHandler(h)
-	// registerHandler := register.NewHandler(h)
-	eventHandler := event.NewHandler(h)
+	eventHandler := eventHandler.New(h)
+	productHandler := productHandler.New(h)
 
-	// TODO: now use the auth in the different handler here
-	mux.Handle("POST /api/v1/register", userHandler.Register())
-
-	// assign to multiplexer
-	// user
-	mux.Handle("GET /api/v1/me", userHandler.GetUser())
-	mux.Handle("PUT /api/v1/me", userHandler.UpdateUser())
-	mux.Handle("DELETE /api/v1/me", userHandler.DeleteUser())
-	mux.Handle(fmt.Sprintf("POST /api/v1/%s", serverutil.SIGNUPENDPOINT), userHandler.CreateAccount())
+	// middlewares declaration
 	rateLimit := mw.PerIPRateLimit(1, 1)
-	mux.Handle(fmt.Sprintf("POST /api/v1/%s", serverutil.SIGNINENDPOINT), rateLimit(userHandler.Signin()))
-	mux.Handle("POST /api/v1/signout", userHandler.Signout())
 
-	mux.Handle("POST /api/v1/oauth/{provider}/user", userHandler.HandleOAuth())
+	// user
+	router.HandleFunc("GET /api/v1/user/me", userHandler.GetUser)
+	router.HandleFunc("PUT /api/v1/user/me", userHandler.UpdateUser)
+	router.HandleFunc("DELETE /api/v1/user/me", userHandler.DeleteUser)
 
-	// payment
-	mux.Handle("POST /api/v1/admin/payment", paymentHandler.CreateEventProduct())
-	mux.Handle("DELETE /api/v1/admin/payment", paymentHandler.DeleteEventProduct())
+	router.HandleFunc("POST /api/v1/user/signin", rateLimit(userHandler.Signin))
+
+	router.HandleFunc("POST /api/v1/user/register", rateLimit(userHandler.RegisterUserOTP))
+	router.HandleFunc("POST /api/v1/user/validate-otp", rateLimit(userHandler.ValidateUserOTP))
+	router.HandleFunc("GET /api/v1/user/approve-user", rateLimit(userHandler.GetUsersToApprove))
+	router.HandleFunc("POST /api/v1/user/approve-user", rateLimit(userHandler.ApproveUserRegistration))
+
+	router.HandleFunc("POST /api/v1/user/signout", userHandler.Signout)
+
+	router.HandleFunc("POST /api/v1/oauth/{provider}", userHandler.HandleOAuth)
 
 	// vote
-	mux.Handle("GET /api/v1/vote/{month}/{year}", voteHandler.GetVotesByUserID())
-	// checkout
-	mux.Handle("POST /api/v1/checkout/{id}/{spot}", checkoutHandler.CreateCheckoutSession())
-	// register
-	// mux.Handle("POST /api/v1/register", registerHandler.MakeRegistration())
-	// event
-	mux.Handle("GET /api/v1/event/{id}", eventHandler.FindEventByID())
+	router.HandleFunc("GET /api/v1/vote/{month}/{year}", voteHandler.GetVotesByUserID)
 
-	s.srv.Handler = mux
+	// register
+	// NOTE: the old way to do the reservation thing
+	// mux.Handle("POST /api/v1/register", registerHandler.MakeRegistration())
+	// TODO: the better way to do the reservation thing
+	// mux.Handle("POST /api/v1/register/event", registerHandler.MakeRegistration())
+	// mux.Handle("POST /api/v1/register/consultation", registerHandler.MakeRegistration())
+
+	// products
+	router.HandleFunc("POST /api/v1/products", productHandler.CreateProduct)
+	router.HandleFunc("GET /api/v1/products/{id}", productHandler.GetProduct)
+	router.HandleFunc("DELETE /api/v1/products/{id}", productHandler.DeleteProduct)
+	router.HandleFunc("PUT /api/v1/products/{id}", productHandler.UpdateProduct)
+
+	// product types
+	router.HandleFunc("POST /api/v1/product-types", productHandler.CreateProductType)
+	router.HandleFunc("DELETE /api/v1/product-types/{id}", productHandler.DeleteProductType)
+
+	// event
+	router.HandleFunc("GET /api/v1/events/{id}", eventHandler.FindEventByID)
+	router.HandleFunc("POST /api/v1/events", eventHandler.CreateEvent)
+	router.HandleFunc("PUT /api/v1/events/{id}", eventHandler.ModifyEvent)
+	router.HandleFunc("DELETE /api/v1/events/{id}", eventHandler.FindEventByID)
+	router.HandleFunc("GET /api/v1/events/users", eventHandler.FindEventsForUser)
+
+	router.HandleFunc("POST /api/v1/upload-image", handleImage)
+
+	s.srv.Handler = router
 }
 
-func sayHello(h *app.App) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// fmt.Fprintln(w, "here I am in the basic thing brother")
-		// here use the logger to do  the things that you want to do brother
-		ctx := r.Context()
-		logger, err := contextutil.GetLoggerFromContext(ctx)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		logger.DebugContext(ctx, "a debug message for the first use")
-		logger.InfoContext(ctx, "here is the first message from the logger inside the handler ")
-		fmt.Fprintln(w, "hello world!")
-	})
+// TODO: how can I make groups for that thing and make sure that I can add as much middleware to a group as I want ?
+
+func handleImage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("here we are in the handle image handler")
+	err := r.ParseMultipartForm(10 << 20) // Limit upload size to 10MB
+	if err != nil {
+		http.Error(w, "Failed to parse form data", http.StatusBadRequest)
+		return
+	}
+
+	file, handler, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "Failed to retrieve image file", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	fmt.Println("the filename that I uploaded is:", handler.Filename)
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("here on the server, someone hit the hello endpoint\n\n")
+	fmt.Fprintln(w, "hello world!")
 }
