085124ddb032ca64d9dc43228b74d5053d52fc7f
GaryHY <henry.gary@hotmail.com>
2025-01-07 08:57:25 +0100

    feat: add Format and Parse function for event and registration domain.
    This function help changing values of pointers to this structs to help
    with application or database operations

---
+++ backend/internal/domain/event/format.go
@@ -0,0 +1,29 @@
+package eventService
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/GaryHY/event-reservation-app/internal/domain"
+)
+
+// Format prepares the Event struct for database storage by populating
+// the BeginAtFormatted and EndAtFormatted fields with formatted values
+// derived from the BeginAt and EndAt fields.
+// This ensures consistency and proper formatting before persisting to the database.
+func (e *Event) Format(ctx context.Context) error {
+	// TODO: use the same function than in registration
+	if e.BeginAt.IsZero() {
+		return domain.NewFormatError("registration StartTime field", errors.New("field is zero"))
+	}
+	e.BeginAtFormatted = e.BeginAt.Format(time.RFC3339)
+	e.BeginAt = time.Time{}
+
+	if e.EndAt.IsZero() {
+		return domain.NewFormatError("registration StartTime field", errors.New("field is zero"))
+	}
+	e.EndAtFormatted = e.EndAt.Format(time.RFC3339)
+	e.EndAt = time.Time{}
+	return nil
+}
---
+++ backend/internal/domain/event/parse.go
@@ -0,0 +1,28 @@
+package eventService
+
+import (
+	"time"
+
+	"github.com/GaryHY/event-reservation-app/internal/domain"
+)
+
+// Parse initializes the Event struct after retrieving data from the database
+// by extracting and converting values from the BeginAtFormatted and EndAtFormatted
+// fields into the BeginAt and EndAt fields.
+// This ensures the Event struct is ready for application-level processing.
+func (e *Event) Parse() error {
+	beginAt, err := time.Parse(time.RFC3339, e.BeginAtFormatted)
+	if err != nil {
+		return domain.NewParsingError("registration StartTime field", err)
+	}
+	e.BeginAt = beginAt
+	e.BeginAtFormatted = ""
+
+	endAt, err := time.Parse(time.RFC3339, e.EndAtFormatted)
+	if err != nil {
+		return domain.NewParsingError("registration StartTime field", err)
+	}
+	e.EndAt = endAt
+	e.EndAtFormatted = ""
+	return nil
+}
