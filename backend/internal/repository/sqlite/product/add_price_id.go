666fe08a3a27863d5d2fa05e83a5bceddfdd451c
GaryHY <henry.gary@hotmail.com>
2025-01-07 17:44:42 +0100

    feat: add AddPriceID productRepository function

--- backend/internal/repository/sqlite/product/add_price_id.go
+++ backend/internal/repository/sqlite/product/add_price_id.go
@@ -1,7 +1,42 @@
 package productRepository
 
-import "context"
+import (
+	"context"
+	"errors"
 
+	rp "github.com/GaryHY/event-reservation-app/internal/repository"
+)
+
+// AddPriceID updates the price_id field for a specific product in the database.
+// It takes a context for cancellation/timeout, a productID to identify the product,
+// and a priceID to be set. Returns an error if the update fails, the product
+// doesn't exist, or if there are any database connectivity issues.
 func (p *Repository) AddPriceID(ctx context.Context, productID, priceID string) error {
+	query := `
+        UPDATE products
+        SET price_id = ?
+        WHERE id = ?;
+    `
+	result, err := p.DB.ExecContext(ctx, query,
+		priceID,
+		productID,
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, context.DeadlineExceeded), errors.Is(err, context.Canceled):
+			return rp.NewContextErr(err)
+		default:
+			return rp.NewDatabaseErr(err)
+		}
+	}
+
+	// Check if the insert was successful
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return rp.NewDatabaseErr(err)
+	}
+	if rowsAffected == 0 {
+		return rp.NewNotUpdatedErr(err, "product")
+	}
 	return nil
 }
