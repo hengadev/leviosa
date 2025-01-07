6996e4560167ea7a9298c3876fc714c18ce9d60b
GaryHY <henry.gary@hotmail.com>
2025-01-07 11:28:31 +0100

    feat: add userService FindAccountByID service

--- backend/internal/domain/user/find_account_by_id.go
+++ backend/internal/domain/user/find_account_by_id.go
@@ -2,25 +2,30 @@ package userService
 
 import (
 	"context"
+	"errors"
 
 	"github.com/GaryHY/event-reservation-app/internal/domain"
 	"github.com/GaryHY/event-reservation-app/internal/domain/user/models"
+	rp "github.com/GaryHY/event-reservation-app/internal/repository"
 )
 
 // FindAccountByID return the decrypted user with the specified ID
 func (s *Service) FindAccountByID(ctx context.Context, userID string) (*models.User, error) {
-	// the encrypted user from the database
 	user, err := s.repo.FindAccountByID(ctx, userID)
 	if err != nil {
 		switch {
+		case errors.Is(err, rp.ErrNotFound):
+			return &models.User{}, nil
+		case errors.Is(err, rp.ErrContext):
+			return nil, err
+		case errors.Is(err, rp.ErrDatabase):
+			return nil, domain.NewQueryFailedErr(err)
 		default:
 			return nil, domain.NewUnexpectTypeErr(err)
 		}
 	}
-	// - decrypt user
-	err = s.DecryptUser(user)
-	if err != nil {
-
+	if errs := s.DecryptUser(user); len(errs) > 0 {
+		return nil, domain.NewInvalidValueErr(err.Error())
 	}
 	return user, nil
 }
