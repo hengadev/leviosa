6fc40dda00eb12492cd70c51ac8724f99f409197
GaryHY <henry.gary@hotmail.com>
2025-01-07 11:12:59 +0100

    feat: add GetPendingUsers userRepository function

--- backend/internal/repository/sqlite/user/get_pending_user.go
+++ backend/internal/repository/sqlite/user/get_pending_user.go
@@ -4,44 +4,55 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 
 	"github.com/GaryHY/event-reservation-app/internal/domain/user/models"
 	rp "github.com/GaryHY/event-reservation-app/internal/repository"
 )
 
-func (u *Repository) GetPendingUser(ctx context.Context, emailHash string) (*models.User, error) {
+func (u *Repository) GetPendingUser(ctx context.Context, emailHash string, provider models.ProviderType) (*models.User, error) {
 	var user models.User
-	query := `
-        SELECT 
-            id,
-            email,
-            password,
-            lastname,
-            firstname,
-            gender,
-            birthdate,
-            telephone,
-            postal_code,
-            city,
-            address1,
-            address2
-        FROM pending_users 
-        WHERE email = ?;`
+	var query string
+	var args []interface{}
+	switch provider {
+	case models.Google:
+	case models.Apple:
+	case models.Mail:
+		query = `
+            SELECT 
+                id,
+                email,
+                password,
+                lastname,
+                firstname,
+                gender,
+                birthdate,
+                telephone,
+                postal_code,
+                city,
+                address1,
+                address2
+            FROM pending_users 
+            WHERE email = ?;`
+		args = []interface{}{
+			&user.ID,
+			&user.EmailHash,
+			&user.PasswordHash,
+			&user.LastName,
+			&user.FirstName,
+			&user.Gender,
+			&user.EncryptedBirthDate,
+			&user.Telephone,
+			&user.PostalCode,
+			&user.City,
+			&user.Address1,
+			&user.Address2,
+		}
+	default:
+		return nil, rp.NewValidationErr(fmt.Errorf("unsupported provider type: %v", provider), "provider")
+	}
 
-	err := u.DB.QueryRowContext(ctx, query, emailHash).Scan(
-		&user.ID,
-		&user.EmailHash,
-		&user.PasswordHash,
-		&user.LastName,
-		&user.FirstName,
-		&user.Gender,
-		&user.EncryptedBirthDate,
-		&user.Telephone,
-		&user.PostalCode,
-		&user.City,
-		&user.Address1,
-		&user.Address2,
-	)
+	err := u.DB.QueryRowContext(ctx, query, emailHash).Scan(args...)
 	if err != nil {
 		switch {
 		case errors.Is(err, sql.ErrNoRows):
--- backend/internal/repository/sqlite/user/get_pending_users.go
+++ backend/internal/repository/sqlite/user/get_pending_users.go
@@ -2,10 +2,52 @@ package userRepository
 
 import (
 	"context"
+	"errors"
 
 	"github.com/GaryHY/event-reservation-app/internal/domain/user/models"
+	rp "github.com/GaryHY/event-reservation-app/internal/repository"
 )
 
 func (u *Repository) GetPendingUsers(ctx context.Context) ([]*models.User, error) {
-	return nil, nil
+	query := `
+        SELECT 
+            email,
+            lastname,
+            firstname,
+            google_id,
+            apple_id
+        FROM users;`
+	rows, err := u.DB.QueryContext(ctx, query)
+	if err != nil {
+		switch {
+		case errors.Is(err, context.DeadlineExceeded), errors.Is(err, context.Canceled):
+			return nil, rp.NewContextErr(err)
+		default:
+			return nil, rp.NewDatabaseErr(err)
+		}
+	}
+	defer rows.Close()
+
+	var users []*models.User
+	for rows.Next() {
+		var user *models.User
+		if err := rows.Scan(
+			&user.EmailHash,
+			&user.LastName,
+			&user.FirstName,
+			&user.GoogleID,
+			&user.AppleID,
+		); err != nil {
+			return nil, rp.NewDatabaseErr(err)
+		}
+		users = append(users, user)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, rp.NewDatabaseErr(err)
+	}
+	if len(users) == 0 {
+		return []*models.User{}, rp.NewNotFoundErr(err, "pending users list")
+	}
+	return users, nil
 }
