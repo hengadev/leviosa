985be6e144d5c69ea2ccceb0a49a9d8b3b2d8c8b
GaryHY <henry.gary@hotmail.com>
2025-01-31 22:10:22 +0100

    build: update to integrate staging environment in frontend code

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import type { Plugin } from 'vite';
+import type { ConfigEnv, Plugin } from 'vite';
 import { kitRoutes } from 'vite-plugin-kit-routes';
 import mkcert from 'vite-plugin-mkcert';
 
@@ -19,13 +19,16 @@ const add_browser_onmount: Plugin = {
     }
 };
 
-export default defineConfig({
-    plugins: [sveltekit(), add_browser_onmount, kitRoutes(), mkcert()],
-    test: {
-        include: ['src/**/*.{test,spec}.{js,ts}', 'tests/**/*.{test,spec}.{js,ts}'],
-        environment: 'jsdom',
-        setupFiles: ['src/vitest.setup.ts'],
-        globals: true
-    },
-    server: { https: true },
+export default defineConfig(({ command, mode }: ConfigEnv) => {
+    const port = (mode === "staging" && command == "build") ? 3001 : 3000;
+    return {
+        plugins: [sveltekit(), add_browser_onmount, kitRoutes(), mkcert()],
+        test: {
+            include: ['src/**/*.{test,spec}.{js,ts}', 'tests/**/*.{test,spec}.{js,ts}'],
+            environment: 'jsdom',
+            setupFiles: ['src/vitest.setup.ts'],
+            globals: true
+        },
+        server: { https: true, port: port },
+    }
 });
