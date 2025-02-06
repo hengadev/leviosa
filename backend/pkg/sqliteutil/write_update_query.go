9b489ee02a83e44e6d8fc68b309a17956da7f66e
GaryHY <henry.gary@hotmail.com>
2025-02-06 12:12:59 +0100

    backend/sqliteutil: add GetProhibitedFields to SQLMappable interface
    This method allows retrieving a list of non-updatable fields for the
    struct it is implemented on.

--- backend/pkg/sqliteutil/write_update_query.go
+++ backend/pkg/sqliteutil/write_update_query.go
@@ -10,13 +10,13 @@ import (
 
 type SQLMappable interface {
 	GetSQLColumnMapping() map[string]string
+	GetProhibitedFields() []string
 }
 
 // Return the necessary elements to write a query update to target the non zero value.
 func WriteUpdateQuery[T SQLMappable](
 	object T,
 	whereMap map[string]any,
-	prohibitedFields ...string,
 ) (string, []any, errsx.Map) {
 	var errs errsx.Map
 	var tables []string
@@ -30,7 +30,7 @@ func WriteUpdateQuery[T SQLMappable](
 	for _, f := range vf {
 		value := v.FieldByName(f.Name)
 		if !value.IsZero() && value.CanInterface() {
-			if err := isProhibitedField(f.Name, prohibitedFields...); err != nil {
+			if err := isProhibitedField(f.Name, object.GetProhibitedFields()); err != nil {
 				notUpdatedFields = append(notUpdatedFields, f.Name)
 				continue
 			}
@@ -59,7 +59,7 @@ func placeholder(name string) string {
 }
 
 // Helper function to check if a struct field belongs to a list of strings provided.
-func isProhibitedField(name string, prohibitedFields ...string) error {
+func isProhibitedField(name string, prohibitedFields []string) error {
 	for _, prohibitedField := range prohibitedFields {
 		if name == prohibitedField {
 			return fmt.Errorf("field %q is prohibited", prohibitedField)
