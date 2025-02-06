e24a877a9b86fe853f3d0cd823dd5eb9e65a33ab
GaryHY <henry.gary@hotmail.com>
2025-02-06 18:18:18 +0100

    backend/userRepository: add TestDeleteUser

--- backend/internal/repository/sqlite/user/delete_user_test.go
+++ backend/internal/repository/sqlite/user/delete_user_test.go
@@ -4,23 +4,42 @@ import (
 	"context"
 	"testing"
 
+	rp "github.com/GaryHY/leviosa/internal/repository"
 	"github.com/GaryHY/leviosa/internal/repository/sqlite"
 	"github.com/GaryHY/leviosa/internal/repository/sqlite/user"
 	"github.com/GaryHY/leviosa/tests/utils/factories"
+	"github.com/google/uuid"
 
 	"github.com/GaryHY/test-assert"
 )
 
 func TestDeleteUser(t *testing.T) {
-	t.Setenv("TEST_MIGRATION_PATH", "../migrations/tests")
+	t.Setenv("TEST_MIGRATION_PATH", "../migrations/test")
+	user := factories.NewBasicUser(nil)
 	tests := []struct {
-		userID  string
-		wantErr bool
-		version int64
-		name    string
+		name          string
+		version       int64
+		userID        string
+		expectedError error
 	}{
-		{userID: factories.Johndoe.ID, wantErr: true, version: 20240811085134, name: "user not in the database"},
-		{userID: "95832", wantErr: true, version: 20240811140841, name: "wrong query"}, {userID: factories.Johndoe.ID, wantErr: false, version: 20240811140841, name: "nominal case"},
+		{
+			name:          "no users in database",
+			version:       20240811085134,
+			userID:        user.ID,
+			expectedError: rp.ErrNotDeleted,
+		},
+		{
+			name:          "ID not found in database",
+			version:       20240811140841,
+			userID:        uuid.NewString(),
+			expectedError: rp.ErrNotDeleted,
+		},
+		{
+			name:          "ID not found in database",
+			version:       20240811140841,
+			userID:        user.ID,
+			expectedError: nil,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -29,7 +48,7 @@ func TestDeleteUser(t *testing.T) {
 			repo, teardown := sqlite.SetupRepository(t, ctx, tt.version, userRepository.New)
 			defer teardown()
 			err := repo.DeleteUser(ctx, tt.userID)
-			assert.Equal(t, err != nil, tt.wantErr)
+			assert.EqualError(t, err, tt.expectedError)
 		})
 	}
 }
