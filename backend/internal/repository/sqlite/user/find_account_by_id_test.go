c0fcad237bdaf249d898af976f0fed8efa0f9415
GaryHY <henry.gary@hotmail.com>
2025-02-06 18:12:54 +0100

    backend/userRepository: add TestFindAccountByID

--- backend/internal/repository/sqlite/user/find_account_by_id_test.go
+++ backend/internal/repository/sqlite/user/find_account_by_id_test.go
@@ -5,23 +5,47 @@ import (
 	"testing"
 
 	"github.com/GaryHY/leviosa/internal/domain/user/models"
+	rp "github.com/GaryHY/leviosa/internal/repository"
 	"github.com/GaryHY/leviosa/internal/repository/sqlite"
 	"github.com/GaryHY/leviosa/internal/repository/sqlite/user"
 	"github.com/GaryHY/leviosa/tests/utils/factories"
+	"github.com/google/uuid"
 
 	"github.com/GaryHY/test-assert"
 )
 
 func TestFindAccountByID(t *testing.T) {
-	t.Setenv("TEST_MIGRATION_PATH", "../migrations/tests")
+	t.Setenv("TEST_MIGRATION_PATH", "../migrations/test")
+	user := factories.NewBasicUser(nil)
+	fields := []string{"Email", "Picture", "Role", "LastName", "FirstName", "Gender", "EncryptedBirthDate", "Telephone", "PostalCode", "City", "Address1", "Address2"}
 	tests := []struct {
-		expectedUser *models.User
-		wantErr      bool
-		version      int64
-		name         string
+		name          string
+		version       int64
+		userID        string
+		expectedUser  *models.User
+		expectedError error
 	}{
-		{expectedUser: nil, wantErr: true, version: 20240811085134, name: "user not in the database"},
-		{expectedUser: factories.Johndoe, wantErr: false, version: 20240811140841, name: "nominal case, user in database"},
+		{
+			name:          "No user in database",
+			version:       20240811085134,
+			userID:        user.ID,
+			expectedUser:  nil,
+			expectedError: rp.ErrNotFound,
+		},
+		{
+			name:          "ID not in database",
+			version:       20240811140841,
+			userID:        uuid.NewString(),
+			expectedUser:  nil,
+			expectedError: rp.ErrNotFound,
+		},
+		{
+			name:          "nominal case with user in database",
+			version:       20240811140841,
+			userID:        user.ID,
+			expectedUser:  user,
+			expectedError: nil,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -29,10 +53,9 @@ func TestFindAccountByID(t *testing.T) {
 			ctx := context.Background()
 			repo, teardown := sqlite.SetupRepository(t, ctx, tt.version, userRepository.New)
 			defer teardown()
-			user, err := repo.FindAccountByID(ctx, "1")
-			assert.Equal(t, err != nil, tt.wantErr)
+			user, err := repo.FindAccountByID(ctx, tt.userID)
+			assert.EqualError(t, err, tt.expectedError)
 			if tt.expectedUser != nil {
-				fields := []string{"ID", "Email", "Role", "BirthDate", "LastName", "FirstName", "Gender", "Telephone", "Address", "City", "PostalCard"}
 				assert.FieldsEqual(t, user, tt.expectedUser, fields)
 			}
 		})
