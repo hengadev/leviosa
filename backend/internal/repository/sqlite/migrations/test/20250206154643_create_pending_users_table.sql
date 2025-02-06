fccb0f14b5af7c3ae8a9c8bdd912534db5e1aa75
GaryHY <henry.gary@hotmail.com>
2025-02-06 17:45:25 +0100

    backend/migrations: add TestGetpendingUser related migrations

---
+++ backend/internal/repository/sqlite/migrations/test/20250206154643_create_pending_users_table.sql
@@ -0,0 +1,27 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS pending_users (
+    id TEXT PRIMARY KEY,
+    email_hash TEXT NOT NULL,
+    encrypted_email TEXT NOT NULL UNIQUE,
+    password_hash TEXT,
+    encrypted_picture TEXT NOT NULL UNIQUE,
+	encrypted_birthdate  TEXT NOT NULL,
+    encrypted_lastname TEXT NOT NULL,
+    encrypted_firstname TEXT NOT NULL,
+	encrypted_gender TEXT NOT NULL,
+	encrypted_telephone TEXT NOT NULL UNIQUE,
+    encrypted_created_at TEXT NOT NULL,
+    encrypted_postal_code TEXT NOT NULL UNIQUE, 
+    encrypted_city TEXT NOT NULL UNIQUE, 
+    encrypted_address1 TEXT NOT NULL UNIQUE, 
+    encrypted_address2 TEXT NOT NULL UNIQUE,
+    encrypted_google_id TEXT,
+    encrypted_apple_id TEXT 
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE pending_users;
+-- +goose StatementEnd
