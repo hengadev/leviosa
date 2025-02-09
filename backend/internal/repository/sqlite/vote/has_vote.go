94b6baeb6a20303cbb94f71421e6a2e9fd2013ae
GaryHY <henry.gary@hotmail.com>
2025-02-09 21:23:04 +0100

    backend/voteRepository: add TestHasVote

--- backend/internal/repository/sqlite/vote/has_vote.go
+++ backend/internal/repository/sqlite/vote/has_vote.go
@@ -2,25 +2,30 @@ package voteRepository
 
 import (
 	"context"
-	"database/sql"
 	"errors"
 
 	rp "github.com/GaryHY/leviosa/internal/repository"
 )
 
 func (v *repository) HasVote(ctx context.Context, month, year int, userID string) error {
-	var res bool
-	query := "SELECT 1 FROM votes WHERE userid=? AND month=? AND year=?;"
-	err := v.DB.QueryRowContext(ctx, query, userID, month, year).Scan(&res)
+	var exists bool
+	query := `
+        SELECT EXISTS (
+            SELECT 1 
+            FROM votes 
+            WHERE user_id = ? AND month = ? AND year = ?
+        )`
+	err := v.DB.QueryRowContext(ctx, query, userID, month, year).Scan(&exists)
 	if err != nil {
 		switch {
-		case errors.Is(err, sql.ErrNoRows):
-			return rp.NewNotFoundErr(err, "votes")
 		case errors.Is(err, context.DeadlineExceeded), errors.Is(err, context.Canceled):
 			return rp.NewContextErr(err)
 		default:
 			return rp.NewDatabaseErr(err)
 		}
+	}
+	if !exists {
+		return rp.NewNotFoundErr(errors.New("no vote found for the user with specified ID"), "votes")
 	}
 	return nil
 }
--- backend/internal/repository/sqlite/vote/has_vote_test.go
+++ backend/internal/repository/sqlite/vote/has_vote_test.go
@@ -2,29 +2,56 @@ package voteRepository_test
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
+	rp "github.com/GaryHY/leviosa/internal/repository"
 	"github.com/GaryHY/leviosa/internal/repository/sqlite"
 	"github.com/GaryHY/leviosa/internal/repository/sqlite/vote"
+	test "github.com/GaryHY/leviosa/tests/utils"
+	"github.com/GaryHY/leviosa/tests/utils/factories"
 
 	"github.com/GaryHY/test-assert"
 )
 
 func TestHasVote(t *testing.T) {
-	t.Setenv("TEST_MIGRATION_PATH", "../migrations/tests")
+	t.Setenv("TEST_MIGRATION_PATH", "../migrations/test")
+	userID := factories.NewBasicUser(nil).ID
 	tests := []struct {
+		name         string
+		version      int64
 		userID       string
 		month        int
 		year         int
 		expectedVote bool
-		wantErr      bool
-		version      int64
-		name         string
+		expectedErr  error
 	}{
-		{userID: "1", month: 4, year: 2025, expectedVote: false, wantErr: true, version: 20240820223653, name: "no vote in db"},
-		{userID: "349324", month: 4, year: 2025, expectedVote: false, wantErr: true, version: 20240820225713, name: "wrong ID"},
-		{userID: "1", month: 4, year: 2025, expectedVote: true, wantErr: false, version: 20240820225713, name: "nominal case"},
+		{
+			name:         "no votes in database",
+			version:      20240820223653,
+			userID:       userID,
+			month:        4,
+			year:         2025,
+			expectedVote: false,
+			expectedErr:  rp.ErrNotFound,
+		},
+		{
+			name:         "wrong ID",
+			version:      20240820225713,
+			userID:       test.GenerateRandomString(16),
+			month:        4,
+			year:         2025,
+			expectedVote: false,
+			expectedErr:  rp.ErrNotFound,
+		},
+		{
+			name:         "nominal case",
+			version:      20240820225713,
+			userID:       userID,
+			month:        4,
+			year:         2025,
+			expectedVote: true,
+			expectedErr:  nil,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -33,8 +60,7 @@ func TestHasVote(t *testing.T) {
 			repo, teardown := sqlite.SetupRepository(t, ctx, tt.version, voteRepository.New)
 			defer teardown()
 			err := repo.HasVote(ctx, tt.month, tt.year, tt.userID)
-			fmt.Println("ther error is:", err)
-			assert.Equal(t, err != nil, tt.wantErr)
+			assert.EqualError(t, err, tt.expectedErr)
 		})
 	}
 }
