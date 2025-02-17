e5a15843967031f3970fe89889ba1daf5ceb4da9
GaryHY <henry.gary@hotmail.com>
2025-02-17 16:48:54 +0100

    backend/factories: add NewBasicProductList

--- backend/tests/utils/factories/product.go
+++ backend/tests/utils/factories/product.go
@@ -23,3 +23,20 @@ func NewBasicProduct(overrides map[string]interface{}) *productService.Product {
 	}
 	return product
 }
+
+func NewBasicProductList() []*productService.Product {
+	products := []*productService.Product{
+		NewBasicProduct(nil),
+		NewBasicProduct(map[string]any{
+			"ID":          "893a7ff5-bc34-438a-a0ed-1d426711e77a",
+			"Name":        "Second product name for Leviosa",
+			"Description": "Second product description for Leviosa",
+		}),
+		NewBasicProduct(map[string]any{
+			"ID":          "019575bc-8494-45d9-9ca2-85bafa86a64f",
+			"Name":        "Third product name for Leviosa",
+			"Description": "Third product description for Leviosa",
+		}),
+	}
+	return products
+}
