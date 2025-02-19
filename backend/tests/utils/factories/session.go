aaf85b29b547d5437830e1b0f2f2e533d81699f7
GaryHY <henry.gary@hotmail.com>
2025-02-19 22:39:50 +0100

    backend/factories: add NewBasicInitSession

--- backend/tests/utils/factories/session.go
+++ backend/tests/utils/factories/session.go
@@ -7,15 +7,10 @@ import (
 	"github.com/GaryHY/leviosa/internal/domain/user/models"
 )
 
-const SessionID = "a36bde82-55ae-4fa9-9289-cb82bde68014"
-const RandomSessionID = "3e3e6273-f118-4259-89a5-abb89cdd7492"
-
-// var sessionTime, _ = time.Parse(time.Layout, "07/12 11:00:00AM '98 -0700")
-
 func NewBasicSession(overrides map[string]interface{}) *sessionService.Session {
 	session := sessionService.Session{
-		ID:        SessionID,
-		UserID:    "user123",
+		ID:        "a36bde82-55ae-4fa9-9289-cb82bde68014",
+		UserID:    "3d20640f-2df6-4e76-81fe-9f6587fd5980",
 		Role:      models.BASIC,
 		ExpiresAt: time.Now().Add(15 * time.Minute),
 	}
@@ -34,13 +29,9 @@ func NewBasicSession(overrides map[string]interface{}) *sessionService.Session {
 	return &session
 }
 
-var BaseSession = sessionService.Session{
-	ID:        SessionID,
-	UserID:    "user123",
-	Role:      models.BASIC,
-	ExpiresAt: time.Now().Add(15 * time.Minute),
-}
-
-var InitSession = map[string]*sessionService.Values{
-	BaseSession.ID: BaseSession.Values(),
+func NewBasicInitSession(overrides map[string]any) map[string]*sessionService.Values {
+	session := NewBasicSession(overrides)
+	return map[string]*sessionService.Values{
+		session.ID: session.Values(),
+	}
 }
