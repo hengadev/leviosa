a7e7573c13d076263d3f3c4cc6598edb9ed2a45d
GaryHY <henry.gary@hotmail.com>
2025-02-22 11:08:47 +0100

    backend/messageRepository: implement basic message repository functionality

---
+++ backend/internal/repository/sqlite/message/create_conversation.go
@@ -0,0 +1,45 @@
+package messageRepository
+
+import (
+	"context"
+	"errors"
+
+	"github.com/GaryHY/leviosa/internal/domain/message/models"
+	rp "github.com/GaryHY/leviosa/internal/repository"
+)
+
+func (m *Repository) CreateConversation(ctx context.Context, conversation *models.Conversation) error {
+	query := `
+        INSERT INTO conversations (
+            id,
+            user_id,
+            admin_id,
+            encrypted_created_at
+        ) VALUES (?, ?, ?, ?);`
+	result, err := m.DB.ExecContext(
+		ctx,
+		query,
+		conversation.ID,
+		conversation.UserID,
+		conversation.AdminID,
+		conversation.EncryptedCreatedAt,
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, context.DeadlineExceeded), errors.Is(err, context.Canceled):
+			return rp.NewContextErr(err)
+		default:
+			return rp.NewDatabaseErr(err)
+		}
+
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return rp.NewDatabaseErr(err)
+	}
+	if rowsAffected == 0 {
+		return rp.NewNotCreatedErr(errors.New("no rows affected by insertion statement"), "conversation")
+	}
+	return nil
+}
