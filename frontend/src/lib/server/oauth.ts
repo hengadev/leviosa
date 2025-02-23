16eed32b5f4f8e41eb59bd31bdabd5e2f85098aa
GaryHY <henry.gary@hotmail.com>
2025-02-23 05:13:16 +0100

    frontend/build: fix GOOGLE_CLIENT_ID and GOOGLE_CLIENT_SECRET import

--- frontend/src/lib/server/oauth.ts
+++ frontend/src/lib/server/oauth.ts
@@ -1,8 +1,15 @@
 import { Google } from 'arctic';
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
+// import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
 
+// export const google = new Google(
+//     GOOGLE_CLIENT_ID,
+//     GOOGLE_CLIENT_SECRET,
+//     'http://localhost:5173/oauth/google/callback'
+// );
+
+console.log("from the oauth thing:", import.meta.env.VITE_GOOGLE_CLIENT_ID)
 export const google = new Google(
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET,
+    import.meta.env.VITE_GOOGLE_CLIENT_ID,
+    import.meta.env.VITE_GOOGLE_CLIENT_SECRET,
     'http://localhost:5173/oauth/google/callback'
 );
