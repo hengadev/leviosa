6f989696f5659f2f07083a34d7a4135d41273989
GaryHY <henry.gary@hotmail.com>
2025-03-10 11:52:02 +0100

    backend/routes: remove API versioning
    - The backend is only consumed by the frontend and they are deployed
    together

--- backend/internal/server/routes.go
+++ backend/internal/server/routes.go
@@ -32,49 +32,49 @@ func (s *Server) addRoutes(h *app.App) {
 	router.HandleFunc("GET /health", healthHandler.CheckHealth)
 
 	// user
-	router.HandleFunc("GET /api/v1/user/me", userHandler.GetUser)
-	router.HandleFunc("PUT /api/v1/user/me", userHandler.UpdateUser)
-	router.HandleFunc("DELETE /api/v1/user/me", userHandler.DeleteUser)
+	router.HandleFunc("GET /user/me", userHandler.GetUser)
+	router.HandleFunc("PUT /user/me", userHandler.UpdateUser)
+	router.HandleFunc("DELETE /user/me", userHandler.DeleteUser)
 
-	router.HandleFunc("POST /api/v1/user/signin", rateLimit(userHandler.Signin))
+	router.HandleFunc("POST /user/signin", rateLimit(userHandler.Signin))
 
-	router.HandleFunc("POST /api/v1/user/register", rateLimit(userHandler.RegisterUserOTP))
-	router.HandleFunc("POST /api/v1/user/validate-otp", rateLimit(userHandler.ValidateUserOTP))
-	router.HandleFunc("GET /api/v1/user/approve-user", rateLimit(userHandler.GetUsersToApprove))
-	router.HandleFunc("POST /api/v1/user/approve-user", rateLimit(userHandler.ApproveUserRegistration))
+	router.HandleFunc("POST /user/register", rateLimit(userHandler.RegisterUserOTP))
+	router.HandleFunc("POST /user/validate-otp", rateLimit(userHandler.ValidateUserOTP))
+	router.HandleFunc("GET /user/approve-user", rateLimit(userHandler.GetUsersToApprove))
+	router.HandleFunc("POST /user/approve-user", rateLimit(userHandler.ApproveUserRegistration))
 
-	router.HandleFunc("POST /api/v1/user/signout", userHandler.Signout)
+	router.HandleFunc("POST /user/signout", userHandler.Signout)
 
-	router.HandleFunc("POST /api/v1/oauth/{provider}", userHandler.HandleOAuth)
+	router.HandleFunc("POST /oauth/{provider}", userHandler.HandleOAuth)
 
 	// vote
-	router.HandleFunc("GET /api/v1/vote/{month}/{year}", voteHandler.GetVotesByUserID)
+	router.HandleFunc("GET /vote/{month}/{year}", voteHandler.GetVotesByUserID)
 
 	// register
 	// NOTE: the old way to do the reservation thing
-	// mux.Handle("POST /api/v1/register", registerHandler.MakeRegistration())
+	// mux.Handle("POST /register", registerHandler.MakeRegistration())
 	// TODO: the better way to do the reservation thing
-	// mux.Handle("POST /api/v1/register/event", registerHandler.MakeRegistration())
-	// mux.Handle("POST /api/v1/register/consultation", registerHandler.MakeRegistration())
+	// mux.Handle("POST /register/event", registerHandler.MakeRegistration())
+	// mux.Handle("POST /register/consultation", registerHandler.MakeRegistration())
 
 	// products
-	router.HandleFunc("POST /api/v1/products", productHandler.CreateProduct)
-	router.HandleFunc("GET /api/v1/products/{id}", productHandler.GetProduct)
-	router.HandleFunc("DELETE /api/v1/products/{id}", productHandler.DeleteProduct)
-	router.HandleFunc("PUT /api/v1/products/{id}", productHandler.UpdateProduct)
+	router.HandleFunc("POST /products", productHandler.CreateProduct)
+	router.HandleFunc("GET /products/{id}", productHandler.GetProduct)
+	router.HandleFunc("DELETE /products/{id}", productHandler.DeleteProduct)
+	router.HandleFunc("PUT /products/{id}", productHandler.UpdateProduct)
 
 	// product types
-	router.HandleFunc("POST /api/v1/product-types", productHandler.CreateOffer)
-	router.HandleFunc("DELETE /api/v1/product-types/{id}", productHandler.DeleteOffer)
+	router.HandleFunc("POST /product-types", productHandler.CreateOffer)
+	router.HandleFunc("DELETE /product-types/{id}", productHandler.DeleteOffer)
 
 	// event
-	router.HandleFunc("GET /api/v1/events/{id}", eventHandler.FindEventByID)
-	router.HandleFunc("POST /api/v1/events", eventHandler.CreateEvent)
-	router.HandleFunc("PUT /api/v1/events/{id}", eventHandler.ModifyEvent)
-	router.HandleFunc("DELETE /api/v1/events/{id}", eventHandler.FindEventByID)
-	router.HandleFunc("GET /api/v1/events/users", eventHandler.FindEventsForUser)
+	router.HandleFunc("GET /events/{id}", eventHandler.FindEventByID)
+	router.HandleFunc("POST /events", eventHandler.CreateEvent)
+	router.HandleFunc("PUT /events/{id}", eventHandler.ModifyEvent)
+	router.HandleFunc("DELETE /events/{id}", eventHandler.FindEventByID)
+	router.HandleFunc("GET /events/users", eventHandler.FindEventsForUser)
 
-	router.HandleFunc("POST /api/v1/upload-image", handleImage)
+	router.HandleFunc("POST /upload-image", handleImage)
 
 	s.srv.Handler = router
 }
